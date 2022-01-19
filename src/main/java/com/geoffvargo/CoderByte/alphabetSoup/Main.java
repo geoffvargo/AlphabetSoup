9970bfcc1d71bec80b1d09519727872db0a24755
geoffvargo <geoff.vargo@stonybrook.edu>
2022-01-18 22:52:48 -0500

    INITIAL COMMIT

---
+++ src/main/java/com/geoffvargo/CoderByte/alphabetSoup/Main.java
@@ -0,0 +1,21 @@
+package com.geoffvargo.CoderByte.alphabetSoup;
+
+import java.util.*;
+
+/**
+ * Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with
+ * the letters in alphabetical order (i.e. hello becomes ehllo). Assume numbers and punctuation symbols will
+ * not be included in the string.
+ */
+class Main {
+	public static void main(String[] args) {
+		// keep this function call here
+		Scanner s = new Scanner(System.in);
+		System.out.print(AlphabetSoup(s.nextLine()));
+	}
+	
+	public static String AlphabetSoup(String str) {
+		// code goes here
+		return str;
+	}
+}
